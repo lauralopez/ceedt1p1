3e2330dab093db44559d914788e5f95c76a2e70a
lauralopez <lauralopez@macpro-7.naves.private>
2013-10-10 11:09:23 +0200

    inicio

---
+++ src/suma/Suma.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma;
+
+/**
+ *
+ * @author lauralopez
+ */
+public class Suma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int variableA = 1;
+        int variableB = 2;
+        int variableC = variableA+variableB;
+        
+        System.out.println( 
+                "Resultado Suma:"+ variableC);
+     }
+}
